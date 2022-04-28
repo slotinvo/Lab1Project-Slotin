6abf684f5a9cefce6ec6ab6f1e112ac4032b0727
slotinvo <slotin.vitya@mail.ru>
2022-04-28 12:48:41 +0300

    Сделал тесты для task1

---
+++ TestProject-task1/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace TestProject_task1
+{
+    [TestClass]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ UnitTests-task1/UnitTest1.cs
@@ -0,0 +1,23 @@
+using Xunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyLib;
+using NUnit.Framework.Internal;
+
+namespace UnitTests_task1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var matrix1 = new[,]
+            {
+                { 1, 1, 4 },
+                { 10, 4, 2 },
+                { 8, 5, 80 }
+            };
+            Assert.AreEqual();
+        }
+    }
+}
